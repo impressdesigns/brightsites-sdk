b7e2010571d257b35f0e2e2795cd03be7340994b
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2025-09-03 13:39:17 +0000

    Ignore secrets lint for sample value
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,6 +7,6 @@ def test_smoke() -> None:
     """Temporary test to check if the tests are running."""
     client = BrightsitesServices(
         base_url="https://subdomain.mybrightsites.com/api/v2.6.1",
-        token="hi",
+        token="hi",  # noqa: S106 -- junk value for tests
     )
     assert client is not None
